bfe17291395723e1a534fde03ee1bb9c52bdc05f
Ciaran <ciaran@pragmaticdigital.co.uk>
2024-01-30 18:23:20 +0000

    added logging

--- scrape.py
+++ scrape.py
@@ -15,17 +15,25 @@ class colors:
     RED = '\033[91m'
     RESET = '\033[0m'
 
+def log(val):
+    with open("log.txt", "a") as f:
+        f.write(val)
+        f.write("\n")
+                
 def crawl_emails(base_url, max_links=50):
     visited_urls = set()
     queue = [base_url]
     base_domain = urlparse(base_url).netloc
     link_count = 0
     all_emails = set()
+    log(f"Going to {base_url}")
 
     while queue and link_count < max_links:
         url = queue.pop(0)
         if url in visited_urls:
             continue
+
+        log(f"Scraping {url}")
 
         try:
             response = requests.get(url)
@@ -48,17 +56,19 @@ def crawl_emails(base_url, max_links=50):
                 page_emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b', page_text)
 
                 if page_emails:
+                    log(f"Found email on {url}")
                     for email in page_emails:
                         all_emails.add(email)
-
+                    
             else:
-                None
-                # print(colors.RED + f"Failed to retrieve {url}. Status code: {response.status_code}")
+                with open("errors.txt", "a") as f:
+                    f.write(f"Failed to retrieve {url}. Status code: {response.status_code}")
+                    f.write("\n")
 
         except Exception as e:
-            # write to log instead
-            None
-            # print(f"An error occurred while fetching {url}: {str(e)}")
+            with open("errors.txt", "a") as f:
+                f.write(f"An error occurred while fetching {url}: {str(e)}")
+                f.write("\n")
 
     if all_emails:
         print(colors.GREEN + f"{all_emails}" + colors.RESET + " found on " + colors.YELLOW + f"{base_domain}")
@@ -67,6 +77,7 @@ def crawl_emails(base_url, max_links=50):
             json.dump(base_url_data, json_file, indent=2)
             json_file.write(",\n")
     else:
+        print(" ")
         print(colors.RED + f"no emails found on {base_domain}")
 
 def get_maps(keyword, location):
@@ -84,20 +95,6 @@ def get_websites(driver, num_results):
             if len(website_urls) >= num_results:
                 break
     return website_urls
-
-def get_websites(driver, num_results):
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    website_urls = []
-    for link in soup.find_all('a', {'data-value': 'Website'}):
-        website_url = link.get('href')
-        if website_url and "https" in website_url:
-            crawl_emails(website_url)
-            sleep(5)
-            website_urls.append(website_url)
-            if len(website_urls) >= num_results:
-                break
-    return website_urls
-
 
 def main(args):
     with open("output.json", "w") as json_file:
@@ -117,15 +114,22 @@ def main(args):
     print("Collecting map data from:", driver.current_url)
 
     website_urls = get_websites(driver, args.num_results)
+    
+    print(" ")
     for url in website_urls:
         print(colors.YELLOW + "Website URL:", url)
     
     sleep(2)
     driver.quit()
-
+    print(" ")
     print(colors.GREEN + "Successfully saved all to output.json")
+
     with open("output.json", "a") as json_file:
         json_file.write("]\n")
+    
+    with open("log.txt", "w") as f:
+        f.write("Cleared log. Please run file again to get logs.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Google Maps Web Scraper')
